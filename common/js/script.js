0bbb6d1f6e097e2d12ad4bbf9e1bee722bec976e
Tomoharu Ito <treeoflife1103@gmail.com>
2017-11-03 12:27:03 +0900

    Tweak the code

--- common/js/script.js
+++ common/js/script.js
@@ -89,8 +89,8 @@ $(function(){
 
     function Menu_set(){
       if(mediaDetect('(max-width:736px)')){
-        if(!($('#Wrap_menuBtn').length)){
           console.log('Say hello!');
+        if(!($('#Wrap_menuBtn').length)){
           $('header').append('<div id="Wrap_menuBtn"><div class="Menu_btn"><a href="#"><div id="Horizontal"></div></a></div></div>');
           $('#Wrap_menuBtn').append($Menu_contents);
 
